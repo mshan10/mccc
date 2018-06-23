2dade7413476e6f1112bfab183fc3fecf1e957cd
mshan10 <mshan@nd.edu>
2018-06-23 19:49:44 -0400

    added home page

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './home.css';
+
+const Home = () => {
+    return (
+        <div class="intro">
+            <h1>Welcome to MCCC<br> Michiana Chinese Christian Church</h1>
+        </div>
+    );
+}
+
+export default Home;
