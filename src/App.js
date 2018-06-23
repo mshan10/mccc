218bbcef995f06e0fab507ba1420833712b8d3fc
mshan10 <mshan@nd.edu>
2018-06-22 21:52:07 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import HeadNav from './components/head_nav';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to MCCC</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <BrowserRouter>
+            <div className="App">
+                <HeadNav />
+
+            </div>
+        </BrowserRouter>
     );
   }
 }
--- src/components/head_nav.js
+++ src/components/head_nav.js
@@ -1,1 +1,13 @@
-//this is head nav component
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './head_nav.css';
+
+const HeadNav = () => {
+    return (
+        <nav className="navbar navbar-light">
+            <Link to="/"><img src="./favicon.png" />MCCC</Link>
+        </nav>
+    );
+}
+
+export default HeadNav;
