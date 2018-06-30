7157653bfa2d46f76c07d87e9a2d2d27353ed252
mshan10 <mshan@nd.edu>
2018-06-29 23:38:06 -0400

    added menu options wip

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,6 +9,13 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import { Manager, Target, Popper } from 'react-popper';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Collapse from '@material-ui/core/Collapse';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Portal from '@material-ui/core/Portal';
+import MenuList from '@material-ui/core/MenuList';
 import './navbar.css';
 
 const styles = theme => ({
@@ -31,9 +38,26 @@ const styles = theme => ({
     },
 });
 
-const Navbar = (props) => {
-    const { classes } = props;
+class MenuListComposition extends React.Component {
+  state = {
+    open: false,
+  };
 
+  handleToggle = () => {
+    this.setState(state => ({ open: !state.open }));
+  };
+
+  handleClose = event => {
+    if (this.target1.contains(event.target) || this.target2.contains(event.target)) {
+      return;
+    }
+
+    this.setState({ open: false });
+  };
+
+const Navbar = (props) => {
+    const { classes } = this.props;
+    const { open } = this.state;
     return (
         <div className={classes.root}>
             <AppBar position="fixed">
@@ -42,6 +66,40 @@ const Navbar = (props) => {
                     <Typography variant="title" color="inherit" className={classes.flex}>
                         MCCC
                     </Typography>
+                    <Manager>
+                      <Target>
+                        <div
+                          ref={node => {
+                            this.target1 = node;
+                          }}
+                        >
+                          <Button
+                            aria-owns={open ? 'menu-list-grow' : null}
+                            aria-haspopup="true"
+                            onClick={this.handleToggle}
+                          >
+                            Toggle Menu Grow
+                          </Button>
+                        </div>
+                      </Target>
+                      <Popper
+                        placement="bottom-start"
+                        eventsEnabled={open}
+                        className={classNames({ [classes.popperClose]: !open })}
+                      >
+                        <ClickAwayListener onClickAway={this.handleClose}>
+                          <Grow in={open} id="menu-list-grow" style={{ transformOrigin: '0 0 0' }}>
+                            <Paper>
+                              <MenuList role="menu">
+                                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                                <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                              </MenuList>
+                            </Paper>
+                          </Grow>
+                        </ClickAwayListener>
+                      </Popper>
+                    </Manager>
                     <Button className={classNames(classes.button, classes.cssRoot)}>About Us</Button>
                     <Button className={classes.button}>Events</Button>
                     <Button variant="contained" color="primary" className={classes.button}>Contact</Button>
