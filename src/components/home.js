f1e266afa057f30ca8e085a9bae133cc4c50f251
mshan10 <mshan@nd.edu>
2018-07-02 21:41:02 -0400

    finished sunday worship schedule

--- src/components/home.js
+++ src/components/home.js
@@ -5,6 +5,14 @@ import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Grid from '@material-ui/core/Grid';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import FontAwesome from 'react-fontawesome';
 
 
 import './home.css';
@@ -14,6 +22,9 @@ const styles = theme => ({
          ...theme.mixins.gutters(),
          paddingTop: theme.spacing.unit * 2,
          paddingBottom: theme.spacing.unit * 2,
+    },
+    standard: {
+        textAlign: 'center',
     },
     paper: {
         margin: '20px 200px',
@@ -29,7 +40,6 @@ const styles = theme => ({
         color: '#34314c',
         paddingTop: 5,
         paddingBottom: 15,
-        fontSize: 40,
         textTransform: 'capitalize',
     },
     title: {
@@ -43,13 +53,36 @@ const styles = theme => ({
         margin: 0,
         letterSpacing: 1.5,
         fontFamily: 'Ubuntu',
-    }
+    },
+    list: {
+        paddingBottom: 0,
+        paddingTop: 0,
+        color: '#47b8e0',
 
+    },
+    grid: {
+        padding: '20px 10px',
+    },
+    icon: {
+        color: '#47b8e0',
+        marginRight: 5,
+    }
 });
 
 class Home extends React.Component {
+    state = {
+       expanded: null,
+     };
+
+     handleChange = panel => (event, expanded) => {
+       this.setState({
+         expanded: expanded ? panel : false,
+       });
+     };
     render() {
     const { classes } = this.props;
+    const { expanded } = this.state;
+
 
         return (
             <div className="home">
@@ -62,14 +95,51 @@ class Home extends React.Component {
                     </Typography>
                 </div>
                 <section className="section">
-                    <Typography variant="headline" className={classes.Typography}>Join us for worship</Typography>
+                    <Typography variant="headline" className={classNames(classes.Typography, classes.church)}>Join us for worship</Typography>
                     <Paper className={classNames(classes.root, classes.paper)} elevation={5}>
-                        <Typography variant="headline">
-                            This is a sheet of paper.
-                        </Typography>
-                        <Typography>
-                            Paper can be used to build surface or other elements for your application.
-                        </Typography>
+                        <Grid container spacing={16} className={classes.grid}>
+                            <Grid item md={6}>
+                                <Typography variant="headline" className={classes.Typography}>Sunday Worship Procedure</Typography>
+                                <List dense="true">
+                                    <ListItem className={classes.list}><FontAwesome name="far fa-church" className={classes.icon}></FontAwesome>Prelude</ListItem>
+                                    <ListItem className={classes.list}><FontAwesome name="far fa-church" className={classes.icon}></FontAwesome>Call to Worship</ListItem>
+                                    <ListItem className={classes.list}><FontAwesome name="far fa-church" className={classes.icon}></FontAwesome>Singspiration</ListItem>
+                                    <ListItem className={classes.list}><FontAwesome name="far fa-church" className={classes.icon}></FontAwesome>Pastoral Prayer</ListItem>
+                                    <ListItem className={classes.list}><FontAwesome name="far fa-church" className={classes.icon}></FontAwesome>Scripture Reading</ListItem>
+                                    <ListItem className={classes.list}><FontAwesome name="far fa-church" className={classes.icon}></FontAwesome>Message by Pastor</ListItem>
+                                    <ListItem className={classes.list}><FontAwesome name="far fa-church" className={classes.icon}></FontAwesome>Responding Hymn</ListItem>
+                                    <ListItem className={classes.list}><FontAwesome name="far fa-church" className={classes.icon}></FontAwesome>Bedediction</ListItem>
+                                    <ListItem className={classes.list}><FontAwesome name="far fa-church" className={classes.icon}></FontAwesome>Welcome & Announcements</ListItem>
+                                    <ListItem className={classes.list}><FontAwesome name="far fa-church" className={classes.icon}></FontAwesome>Postlude</ListItem>
+                                </List>
+                            </Grid>
+                            <Grid item md={6}>
+                            <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleChange('panel1')}>
+                              <ExpansionPanelSummary>
+                                <Typography className={classes.heading}>English Schedule</Typography>
+                              </ExpansionPanelSummary>
+                              <ExpansionPanelDetails>
+                                <Typography>
+                                  Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+                                  maximus est, id dignissim quam.
+                                </Typography>
+                              </ExpansionPanelDetails>
+                            </ExpansionPanel>
+                            <ExpansionPanel expanded={expanded === 'panel2'} onChange={this.handleChange('panel2')}>
+                              <ExpansionPanelSummary>
+                                <Typography className={classes.heading}>Chinese Schedule</Typography>
+                              </ExpansionPanelSummary>
+                              <ExpansionPanelDetails>
+                                <Typography>
+                                  Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
+                                  diam eros in elit. Pellentesque convallis laoreet laoreet.
+                                </Typography>
+                              </ExpansionPanelDetails>
+                            </ExpansionPanel>
+
+
+                            </Grid>
+                        </Grid>
                     </Paper>
                 </section>
             </div>
